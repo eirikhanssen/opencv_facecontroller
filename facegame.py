e9c757b1aec500ed6148fa2a3daf8767f761f0d7
Eirik Hanssen <eirikhanssen79@gmail.com>
2016-03-23 16:49:38 +0100

    fix size of detected face rectangle

--- facegame.py
+++ facegame.py
@@ -12,6 +12,8 @@ import common #clock
 
 w = 800
 h = 600
+detected_top_left = None
+detected_bottom_right = None
 bigcloud_x = w
 small_clouds_x = w
 speed_big_cloud = 3
@@ -21,24 +23,24 @@ big_cloud_width = 549
 chicken_y_pos = h/2
 _image_library = {}
 
-width = 640
-halfwidth = int(width/2)
-height = 480
+cam_width = 640
+halfcam_width = int(cam_width/2)
+cam_height = 480
 middle_x = None
 middle_y = h/2
 middle = None
-left_x = width/2
+left_x = cam_width/2
 right_x = None
 top_y = None
 bottom_y = None
-center_point = (width/2,height/2)
+center_point = (cam_width/2,cam_height/2)
 vis = None
 x_ratio = None
-proximity_ratio = 1 # height of tracked face rectangle / height of webcam image, initially defaults to 1
+proximity_ratio = 1 # cam_height of tracked face rectangle / cam_height of webcam image, initially defaults to 1
 speed_x_multiplier = 1
 height_diff = None
 score = 0
-upper_threshold = int(0.4 * height)
+upper_threshold = int(0.4 * cam_height)
 args, video_src = getopt.getopt(sys.argv[1:], '', ['cascade=', 'nested-cascade='])
 try: video_src = video_src[0]
 except: video_src = 0
@@ -61,14 +63,28 @@ def textOnScreen(screen,message, x, y):
     screen.blit(textrim, (x-2, y-2))
     screen.blit(text, (x, y))
 
-def draw_face_detect(surf, edge_left, edge_right,edge_top, edge_bottom):
+def draw_face_detect(surf, topleft, bottomright):
+    print("topleft", topleft)
+    print("bottomright: ", bottomright)
     color = (128,255,128)
     thickness = 5
-    edge_left = edge_left * (w/width)
-    edge_right = edge_right * (w/width)
-    edge_top = edge_top * (h/height)
-    edge_bottom = edge_bottom * (h/height)
-    pygame.draw.lines(surf, color, False, [(edge_left, edge_top), (edge_right, edge_top), (edge_right, edge_bottom), (edge_left, edge_bottom), (edge_left, edge_top)], thickness)
+    warn = 0.15
+    warn2 = 0.07
+    if topleft[0] < warn or bottomright[0] > (1-warn) or topleft[1] < warn or bottomright[1] > (1-warn):
+        color = (255,128,255)
+        thickness = 3
+
+    if topleft[0] < warn2 or bottomright[0] > (1-warn2) or topleft[1] < warn2 or bottomright[1] > (1-warn2):
+        color = (255,0,0)
+        thickness = 3
+
+    global w
+    global h
+    tl = [topleft[0]*w, topleft[1]*h]
+    tr = [bottomright[0]*w, topleft[1]*h]
+    bl = [topleft[0]*w, bottomright[1]*h]
+    br = [bottomright[0]*w, bottomright[1]*h]
+    pygame.draw.lines(surf, color, False, [tl, tr, br, bl, tl], thickness)
 
 def get_image(path):
     global _image_library
@@ -98,11 +114,9 @@ while not done:
 
     t = common.clock()
     rects = detect(gray, cascade)
-    #vis = img.copy()
     try:
         coordinates = rects[0]
     except:
-        #print("GAME PAUSED... (face is out of range)")
         face_detected = False
 
         if face_not_detected_timer == None:
@@ -119,6 +133,10 @@ while not done:
         game_paused = False
         face_not_detected_timer = None
         face_detected = True
+
+        detected_top_left = (float(coordinates[0])/cam_width, float(coordinates[1])/cam_height)
+        detected_bottom_right = (float(coordinates[2])/cam_width, float(coordinates[3])/cam_height)
+
         left_x = coordinates[0]
         right_x = coordinates[2]
         top_y = coordinates[1]
@@ -126,7 +144,7 @@ while not done:
         middle_x = int((right_x - left_x)/2 + left_x)
         middle_y = int((bottom_y - top_y)/2 + top_y)
         center_point = (middle_x, middle_y)
-        x_ratio = (center_point[0]-0.25*width)/halfwidth
+        x_ratio = (center_point[0]-0.25*cam_width)/halfcam_width
         if x_ratio < 0:
             x_ratio = 0
         if x_ratio > 1:
@@ -134,38 +152,25 @@ while not done:
             print("x-ratio: " + str(x_ratio))
         print("bottom and top y: " + str(bottom_y) + "," + str(top_y))
         height_diff = int(bottom_y-top_y)
-        proximity_ratio = float(height_diff)/float(height)
+        proximity_ratio = float(height_diff)/float(cam_height)
         print("height ratio: " + str(proximity_ratio))
         speed_x_multiplier = 1.5*x_ratio + 0.5
         print("speed multiplier: " + str(speed_x_multiplier)) #0.5 to 2.0
         print(str("center: " + str(center_point)))
         if center_point[1] < upper_threshold:
-            #draw_circle(vis, center_point, (255,0,0), proximity_ratio)
-            #jump()
             print("flying high")
         else:
-            #draw_circle(vis, center_point, (0,0,255), proximity_ratio)
             print("not drawing circle..")
     finally:
-        #draw_rects(vis, rects, (0, 255, 0))
-        #draw_str(vis, (int(width/2)-50,int(height - height/6)), 'Speed: %.1f' % speed_x_multiplier)
         print("draw the speed multiplier")
 
 
     dt = common.clock() - t
 
-    #draw_str(vis, (20, 20), 'time: %.1f ms' % (dt*1000))
- #   if speed_x_multiplier >= 0.5:
- #       score = score + dt*2**(1+speed_x_multiplier*2.5)
- #   else :
- #       draw_str(vis, (int(width/2)-50,int(height/2)), 'PAUSED!')
-#
-    #draw_str(vis, (20,50), 'Score: %.1f' % score)
+    # facecontroller code end
 
-    #cv2.imshow('facedetect', vis)
-# facecontroller code end
-    print(type(middle_y), type(h), type(height))
-    chicken_y_pos = middle_y * (h/height)
+    print(type(middle_y), type(h), type(cam_height))
+    chicken_y_pos = middle_y * (h/cam_height)
 
     screen.fill((255, 255, 255))
     screen.blit(get_image('images/clearsky.png'), (0, 0))
@@ -175,11 +180,11 @@ while not done:
     textOnScreen(screen, "Score: " + str(int(score)), 40, 0)
     textOnScreen(screen, "Speed: " + str(round(speed_x_multiplier, 2)), 40, h-110)
     if face_detected:
-        draw_face_detect(screen, left_x, right_x, top_y, bottom_y)
+        draw_face_detect(screen, detected_top_left, detected_bottom_right)
 
     if game_paused:
         textOnScreen(screen, "PAUSED!", w/4, h/2)
-
+        textOnScreen(screen, "(Can't see any players)", w/4, h*0.6)
     else: # the game is not paused!
         score = score + dt*2**(1+speed_x_multiplier*2.5)
         if bigcloud_x < -big_cloud_width:
